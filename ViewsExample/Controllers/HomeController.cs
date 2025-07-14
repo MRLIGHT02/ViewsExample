d4837e5ae4a185925d076734d90f6eb0ca04b813
MRLIGHT02 <saifali42020@gmail.com>
2025-07-14 12:27:56 +0530

    #

--- ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/Controllers/HomeController.cs
@@ -35,5 +35,46 @@ namespace ViewsExample.Controllers
 
             return View(person);
         }
+
+        /// <summary>
+        /// Returns the details of a person based on the provided name.
+        /// </summary>
+        /// <remarks>The method searches a predefined list of persons and returns the details of the first
+        /// person whose name matches the provided name.</remarks>
+        /// <param name="name">The name of the person whose details are to be retrieved. This parameter is optional and can be null.</param>
+        /// <returns>An <see cref="IActionResult"/> that renders a view displaying the details of the matching person. If no
+        /// person matches the provided name, the view will display null.</returns>
+
+        [Route("home-person/{name}")]
+        public IActionResult PersonDetails(string? name)
+        {
+
+            List<Person> person = new List<Person>
+{
+        new Person
+    {
+        Name = "Raju",
+        DateOfBirth = DateTime.Parse("2000-12-20").Date,
+        PersonGender = Gender.Male
+    },
+    new Person
+    {
+        Name = "Seema",
+        DateOfBirth = null, //DateTime.Parse("1999-05-10"),
+        PersonGender = Gender.Female
+    },
+    new Person
+    {
+        Name = "Alexa",
+        DateOfBirth = DateTime.Parse("2001-03-15"),
+        PersonGender = Gender.Other
+    }
+
+    };
+
+            Person? MatchingPerson = person.Where(p => p.Name == name).FirstOrDefault();
+
+            return View(MatchingPerson);
+        }
     }
 }
